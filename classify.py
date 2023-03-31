c47a60127c1920482b9bd14ed00eb48e8f71c3fc
ernesto <evoliveira@inf.ufrgs.br>
2023-03-31 17:00:20 -0300

    Reformat project structure

--- classify.py
+++ classify.py
@@ -1,44 +1,12 @@
 import os
-import numpy as np
 import cv2
 import math
 
-GT_DIR = 'ground_truth'
+from preprocess import zero_pad_image
+
+GT_DIR = 'ground_truth/chess_com_default'
 MAX = 10000000
-INPUT_IMAGE = 'ground_truth/bishop/gt2.png'
-
-def zero_pad_image(img, shape):
-    if img.shape == shape:
-        return img
-
-    padded_img = np.full(shape, (0,0,0), dtype=np.uint8)
-
-    x_center = (shape[1] - img.shape[1]) // 2
-    y_center = (shape[0] - img.shape[0]) // 2
-    padded_img[y_center:y_center+img.shape[0], 
-             x_center:x_center+img.shape[1]] = img
-
-    return padded_img
-
-def get_avg_shape(ground_truths_path: str):
-    dim1 = 0
-    dim2 = 0
-    gt_images = os.listdir(ground_truths_path)
-
-    for gt_image_name in gt_images:
-        img = cv2.imread(ground_truths_path + gt_image_name)
-        dim1 = max(dim1, img.shape[0])
-        dim2 = max(dim2, img.shape[1])
-
-    output_shape = (max(dim1, dim2), max(dim1, dim2), 3)
-    imgs_sum = np.zeros(output_shape)
-
-    for gt_image_name in gt_images:
-        gt_image = cv2.imread(ground_truths_path + gt_image_name)
-        gt_image = zero_pad_image(gt_image, output_shape)
-        imgs_sum += gt_image
-
-    return imgs_sum/len(gt_images)
+INPUT_IMAGE = 'ground_truth/chess_com_default/pawn/gt2.png'
 
 def sum_squared_diff(img1, img2):
     sum = 0
@@ -70,13 +38,6 @@ def classify(img):
     return label
 
 def main():
-    directories = os.listdir(GT_DIR)
-    for piece in directories:
-        if piece != '.DS_Store':
-            piece_dir = f'{GT_DIR}/{piece}/'
-            ans = get_avg_shape(piece_dir)
-            cv2.imwrite(f'{piece_dir}avg.png', ans)
-
     input_image = cv2.imread(INPUT_IMAGE)
     label = classify(input_image)
     print('')
--- preprocess.py
+++ preprocess.py
@@ -2,19 +2,20 @@ import numpy as np
 import cv2
 import sys
 import os
+from pathlib import Path
 
-OUTPUT_DIR = 'ground_truth'
+GT_DIR = 'ground_truth'
 
 # Thought about creating an Enum for pieces 
 # settled on using strings for simplicity
 #
 POSITIONS = {
-    'PAWN':     [(x,y) for x in range(8) for y in [1,6]],
-    'ROOK':     [(0,0), (7,0), (0,7), (7,7)],
-    'KNIGHT':   [(1,0), (6,0), (1,7), (6,7)],
-    'BISHOP':   [(2,0), (5,0), (2,7), (5,7)],
-    'QUEEN':    [(3,0), (3,7)],
-    'KING':     [(4,0), (4,7)]
+    'pawn':     [(x,y) for x in range(8) for y in [1,6]],
+    'rook':     [(0,0), (7,0), (0,7), (7,7)],
+    'knight':   [(1,0), (6,0), (1,7), (6,7)],
+    'bishop':   [(2,0), (5,0), (2,7), (5,7)],
+    'queen':    [(3,0), (3,7)],
+    'king':     [(4,0), (4,7)]
 }
 
 
@@ -54,9 +55,9 @@ def get_output_filename(directory):
 # Creates ground truth image files
 # We can later create directories for different themes too
 #
-def save_images(chessboard):
+def save_images(chessboard, theme_name):
     for piece_name, position_list in POSITIONS.items():
-        directory = f'{OUTPUT_DIR}/{piece_name.lower()}'
+        directory = f'{GT_DIR}/{theme_name}/{piece_name}'
         if not os.path.exists(directory):
             os.makedirs(directory)
 
@@ -67,13 +68,55 @@ def save_images(chessboard):
             cv2.imwrite(filename, edge_map)
 
 
+def zero_pad_image(img, shape):
+    if img.shape == shape:
+        return img
+
+    padded_img = np.full(shape, (0,0,0), dtype=np.uint8)
+
+    x_center = (shape[1] - img.shape[1]) // 2
+    y_center = (shape[0] - img.shape[0]) // 2
+    padded_img[y_center:y_center+img.shape[0],
+             x_center:x_center+img.shape[1]] = img
+
+    return padded_img
+
+def get_avg_shape(ground_truths_path: str):
+    dim1 = 0
+    dim2 = 0
+    gt_images = os.listdir(ground_truths_path)
+
+    for gt_image_name in gt_images:
+        img = cv2.imread(ground_truths_path + gt_image_name)
+        dim1 = max(dim1, img.shape[0])
+        dim2 = max(dim2, img.shape[1])
+
+    output_shape = (max(dim1, dim2), max(dim1, dim2), 3)
+    imgs_sum = np.zeros(output_shape)
+
+    for gt_image_name in gt_images:
+        gt_image = cv2.imread(ground_truths_path + gt_image_name)
+        gt_image = zero_pad_image(gt_image, output_shape)
+        imgs_sum += gt_image
+
+    return imgs_sum/len(gt_images)
+
+def calculate_averages(theme_name):
+    for piece in POSITIONS:
+        piece_dir = f'{GT_DIR}/{theme_name}/{piece}/'
+        if os.path.isdir(piece_dir):
+            avg = get_avg_shape(piece_dir)
+            cv2.imwrite(f'{piece_dir}avg.png', avg)
+
 def main():
     filename = sys.argv[1]
     img = cv2.imread(filename)
+    theme_name = Path(filename).stem
     found, corners = cv2.findChessboardCorners(img, (7,7))
     if found:
         chessboard = read_chessboard(img, corners)
-        save_images(chessboard)
+        save_images(chessboard, theme_name)
+        calculate_averages(theme_name)
 
 
 if __name__ == '__main__':
