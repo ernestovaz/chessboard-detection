d1b2b2fca7060d873db35595affd6efac7bc481b
ernesto <evoliveira@inf.ufrgs.br>
2023-04-03 18:35:09 -0300

    Add script for train test splitting

---
+++ train_test_split.sh
@@ -0,0 +1,23 @@
+i=0
+for file in data/*; do 
+  python preprocess.py $file
+  ((i+=1))
+  if (($i == 108))
+  then
+    mkdir ground_truth/test
+    mv ground_truth/pawn ground_truth/test
+    mv ground_truth/rook ground_truth/test
+    mv ground_truth/king ground_truth/test
+    mv ground_truth/queen ground_truth/test
+    mv ground_truth/bishop ground_truth/test
+    mv ground_truth/knight ground_truth/test
+  fi
+done
+
+mkdir ground_truth/train
+mv ground_truth/pawn ground_truth/train
+mv ground_truth/rook ground_truth/train
+mv ground_truth/king ground_truth/train
+mv ground_truth/queen ground_truth/train
+mv ground_truth/bishop ground_truth/train
+mv ground_truth/knight ground_truth/train
